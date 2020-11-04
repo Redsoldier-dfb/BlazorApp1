53e5da28727230f187a4fa580057a8ce121be3a6
David Frías <josefri@hotmail.com>
2020-11-04 20:01:39 +0100

    Agregar archivos de proyecto.

---
+++ BlazorApp1/wwwroot/EventoFrame.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener('message', newTokenizeSyncResponse, false);
+
+function newTokenizeSyncResponse(param) {
+    let data = param.data;
+
+    console.log(data);
+
+    window.removeEventListener('message', newTokenizeSyncResponse);
+
+    DotNet.invokeMethod('BlazorApp1', 'GetIframRS',data);
+};
